9c0ba4a7d718e2795c6abc101aa648d4bce06104
John Pathadan <john.paul@vonnue.com>
2023-12-18 17:34:46 +0530

    fix: activity indicator added

--- App.js
+++ App.js
@@ -1,13 +1,30 @@
+import { ActivityIndicator, StyleSheet, View } from 'react-native'
+import React, { useState } from 'react'
+
 import { NavigationContainer } from '@react-navigation/native'
-import React from 'react'
 import Tabs from './components/Tabs'
 
 const App = () => {
+  const [loading, setLoading] = useState(true)
+  if (loading) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size={'large'} color={'blue'} />
+      </View>
+    )
+  }
   return (
     <NavigationContainer>
       <Tabs />
     </NavigationContainer>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    flex: 1
+  }
+})
 
 export default App
