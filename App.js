a02a2492425ab94847ea8c201830f1a22c369bcc
John Pathadan <john.paul@vonnue.com>
2023-12-18 16:56:14 +0530

    fix: bottom tabs added

--- App.js
+++ App.js
@@ -2,21 +2,65 @@ import { StyleSheet, View } from 'react-native'
 
 import City from './screens/City'
 import CurrentWeather from './screens/CurrentWeather'
+import { Feather } from '@expo/vector-icons'
+import { NavigationContainer } from '@react-navigation/native'
 import React from 'react'
 import UpcomingWeather from './screens/UpcomingWeather'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+
+const Tab = createBottomTabNavigator()
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <City />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={{
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'grey'
+        }}
+      >
+        <Tab.Screen
+          name={'current'}
+          component={CurrentWeather}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Feather
+                name={'droplet'}
+                size={25}
+                color={focused ? 'tomato' : 'black'}
+              />
+            )
+          }}
+        />
+        <Tab.Screen
+          name={'Upcoming'}
+          component={UpcomingWeather}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Feather
+                name={'clock'}
+                size={25}
+                color={focused ? 'tomato' : 'black'}
+              />
+            )
+          }}
+        />
+        <Tab.Screen
+          name={'City'}
+          component={City}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Feather
+                name={'home'}
+                size={25}
+                color={focused ? 'tomato' : 'black'}
+              />
+            )
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  }
-})
 
 export default App
